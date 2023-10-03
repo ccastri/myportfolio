2e6889e18387d4132fa076973b3c782eb0141f6a
ccastri <camilo.castrillon@uao.edu.co>
2023-10-03 11:25:47 -0500

    Some extra fixings in ongoing projects

--- src/app/_components/Stack.tsx
+++ src/app/_components/Stack.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const Stack = () => {
   return (
     <div className="h-screen flex relative text-[#fafafa] bg-black flex-col items-center text-center justify-center w-screen border-2 px-2 ">
-         <div className="absolute  w-screen h-full flex flex-col items-center justify-center space-y-32 z-10">
-         <Image className="w-full absolute h-auto  object-cover z-10 " src="/hip.gif" alt="Unity Logo" width={200} height={200}/>
+         <div className="absolute  w-full h-full flex flex-col items-center justify-center space-y-32 z-10">
+         <Image className="w-auto absolute h-auto  object-cover z-10 " src="/hip.gif" alt="Unity Logo" width={200} height={200}/>
          <h1 className='absolute text-center z-30  font-bold text-[#fafafa] text-xs animate-rotate360'>3D model from a <br/> real patient brain</h1>
          </div>
         {/* <div className="-z-10 absolute h-full   bg-[] clip-triangle -rotate-180 overflow-y-hidden w-screen" /> */}
