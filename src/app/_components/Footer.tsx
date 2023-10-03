2b6298be6ef337220f1523c0d2cb140e10c9cd95
ccastri <camilo.castrillon@uao.edu.co>
2023-10-03 13:17:32 -0500

    slider Images and other skills missing

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -14,13 +14,16 @@ const Footer = () => {
     return `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   };
   return (
-    <main className=' flex flex-col space-y-2 py-8 w-screen items-center justify-center text-slate-700 bg-[#c3d4f5] '>
+    <main className=' flex flex-col space-y-2 py-8 px-4 w-screen items-center justify-center text-slate-700 bg-[#c3d4f5] '>
       {/* <h1>Footer works</h1> */}
       <h1 className="text-2xl ">Just hit me up if anything...</h1>
       <h2 className="px-10 text-center">{`I'll be happy to hear how I'd be helpful to you or how we can collaborate this time`}</h2>
-      <div className="cursor-pointer text-slate-700 rounded w-36 px-4 m-auto flex space-x-2 py-3 bg-[#fafafa] border-2"><LinkedInIcon onClick={()=>window.open('https://www.linkedin.com/camilo-castrillonn', '_blank')}/><span className=" w-full">LinkedIn</span></div>
-      <div className="cursor-pointer text-slate-700 rounded w-36 px-4 m-auto flex space-x-2 py-3 bg-[#fafafa] border-2"><GitHubIcon onClick={()=>window.open('https://www.github.com/ccastri', '_blank')}/><span className=" w-full">Github</span></div>
-      <div className="cursor-pointer text-slate-700 rounded w-36 px-4 m-auto flex space-x-2 py-3 bg-[#fafafa] border-2"><WhatsAppIcon onClick={()=>window.open(generateWhatsAppLink(), '_blank')}/><span className=" w-full">WhatsApp</span></div>
+      <div className="w-full space-y-4 flex flex-col sm:flex-row">
+
+      <div className="cursor-pointer hover:bg-blue-400 hover:text-[#fafafa] rounded w-36 px-4 m-auto flex space-x-2 py-3 bg-[#fafafa] border-2"><LinkedInIcon onClick={()=>window.open('https://www.linkedin.com/camilo-castrillonn', '_blank')}/><span className=" w-full">LinkedIn</span></div>
+      <div className="cursor-pointer hover:bg-slate-700 hover:text-[#fafafa] rounded w-36 px-4 m-auto flex space-x-2 py-3 bg-[#fafafa] border-2"><GitHubIcon onClick={()=>window.open('https://www.github.com/ccastri', '_blank')}/><span className=" w-full">Github</span></div>
+      <div className="cursor-pointer hover:bg-green-500 hover:text-[#fafafa] rounded w-36 px-4 m-auto flex space-x-2 py-3 bg-[#fafafa] border-2"><WhatsAppIcon onClick={()=>window.open(generateWhatsAppLink(), '_blank')}/><span className=" w-full">WhatsApp</span></div>
+      </div>
     </main>
   )
 }
