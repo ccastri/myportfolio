2c6a774d7a855c6ddc479a5f4156e9f2dfb9c292
ccastri <camilo.castrillon@uao.edu.co>
2023-10-03 20:03:08 -0500

    Scrolling animation done

--- src/app/_components/OtherSkills.tsx
+++ src/app/_components/OtherSkills.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const OtherSkills = () => {
   return (
-    <div>
+    <div className='h-screen'>
       <h1>Other skills works</h1>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,63 @@
-import Image from 'next/image'
-import Hero from './Hero'
-import Stack from './_components/Stack'
-import OtherSkills from './_components/OtherSkills'
-import Slider from './_components/Slider'
+'use client'
+import Hero from './Hero';
+import Stack from './_components/Stack';
+import OtherSkills from './_components/OtherSkills';
+import Slider from './_components/Slider';
+import { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 
 export default function Home() {
+  const stackControls = useAnimation();
+  const sliderControls = useAnimation();
+  const otherSkillsControls = useAnimation();
+
+  useEffect(() => {
+    const stackOffset = window.innerHeight / 2;
+    const sliderOffset = window.innerHeight * 1.7; // Change this offset as needed
+    const otherSkillsOffset = window.innerHeight * 2.8; // Change this offset as needed
+
+    const scrollListener = () => {
+      const isStackReached = window.scrollY > stackOffset;
+      const isSliderReached = window.scrollY > sliderOffset;
+      const isOtherSkillsReached = window.scrollY > otherSkillsOffset;
+
+      if (isStackReached) {
+        stackControls.start({ opacity: 1 });
+      } else {
+        stackControls.start({ opacity: 0 });
+      }
+
+      if (isSliderReached) {
+        sliderControls.start({ opacity: 1 });
+      } else {
+        sliderControls.start({ opacity: 0 });
+      }
+
+      if (isOtherSkillsReached) {
+        otherSkillsControls.start({ opacity: 1 });
+      } else {
+        otherSkillsControls.start({ opacity: 0 });
+      }
+    };
+
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    };
+  }, [stackControls, sliderControls, otherSkillsControls]);
+
   return (
-    <main className="flex  flex-col items-center justify-between">
-     <Hero/>
-     <Stack/>
-     <Slider/>
-     <OtherSkills/>
+    <main className="flex flex-col items-center justify-between">
+      <Hero />
+      <motion.div initial={{ opacity: 0 }} animate={stackControls}>
+        <Stack />
+      </motion.div>
+      <motion.div initial={{ opacity: 0 }} animate={sliderControls}>
+        <Slider />
+      </motion.div>
+      <motion.div initial={{ opacity: 0 }} animate={otherSkillsControls}>
+        <OtherSkills />
+      </motion.div>
     </main>
-  )
+  );
 }
