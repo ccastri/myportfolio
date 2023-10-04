7a24da52304b86f9f077b754d2e941f8ff2b8df7
ccastri <camilo.castrillon@uao.edu.co>
2023-10-04 18:36:28 -0500

    doughnut configured at chartjs2

--- src/app/_components/progressBar.tsx
+++ src/app/_components/progressBar.tsx
@@ -1,12 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Doughnut, Chart } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart } from 'chart.js';
+// import { Chart } from 'chart.js'; // Import Chart from 'chart.js/auto'
+
+// Chart.register(...registerables);
+
+interface ICircularProgressBar {
+  items: string | string[];
+}
+
+type ChartRefType = Chart| null;
 
 const CircularProgressBar = () => {
   const [tsProgress, setTsProgress] = useState(0);
   const [jsProgress, setJsProgress] = useState(0);
   const [pythonProgress, setPythonProgress] = useState(0);
 
-  const chartRef = useRef(null); // Reference to the chart instance
+  const chartRef = useRef<ChartRefType | null>(null); // Reference to the chart instance
 
   useEffect(() => {
     const tsInterval = setInterval(() => {
@@ -42,42 +52,45 @@ const CircularProgressBar = () => {
 
   useEffect(() => {
     // Create or update the chart
-    if (chartRef.current) {
-      chartRef.current.data.datasets[0].data = [tsProgress, jsProgress, pythonProgress];
-      chartRef.current.update();
-    } else {
-      const ctx = document.getElementById('chart-canvas').getContext('2d');
-      chartRef.current = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-          labels: ['TypeScript', 'JavaScript', 'Python'],
-          datasets: [
-            {
-              data: [tsProgress, jsProgress, pythonProgress],
-              backgroundColor: [
-                'rgba(75, 192, 192, 0.6)',
-                'rgba(255, 99, 132, 0.6)',
-                'rgba(255, 205, 86, 0.6)',
-              ],
-              borderWidth: 0,
+    const canvas = document.getElementById('chart-canvas') as HTMLCanvasElement;
+    if (canvas) {
+      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+      if (chartRef.current) {
+        chartRef.current!.data.datasets![0].data = [tsProgress, jsProgress, pythonProgress];
+        chartRef.current.update();
+      } else {
+        chartRef.current = new Chart(ctx, {
+          type: 'doughnut',
+          data: {
+            labels: ['TypeScript', 'JavaScript', 'Python'],
+            datasets: [
+              {
+                data: [tsProgress, jsProgress, pythonProgress],
+                backgroundColor: [
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(255, 205, 86, 0.6)',
+                ],
+                borderWidth: 0,
+              },
+            ],
+          },
+          options: {
+            cutoutPercentage: 50,
+            rotation: -0.5 * Math.PI,
+            circumference: 	2 * Math.PI,
+            plugins: {
+              legend: {
+                display: true,
+              },
             },
-          ],
-        },
-        options: {
-          cutout: '50%',
-          rotation: 0,
-          circumference: 360,
-          plugins: {
-            legend: {
-              display: true,
+            animation: {
+              animateRotate: true,
+              animateScale: true,
             },
           },
-          animation: {
-            animateRotate: true,
-            animateScale: true,
-          },
-        },
-      });
+        });
+      }
     }
   }, [tsProgress, jsProgress, pythonProgress]);
 
