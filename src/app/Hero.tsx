bc494632e0aab217f22267a06737e5729aae9ad2
ccastri <camilo.castrillon@uao.edu.co>
2023-10-07 13:30:58 -0500

    hero section looks A W E S O M E

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -7,12 +7,18 @@ import GitHub from '@mui/icons-material/GitHub';
 import DownloadIcon from '@mui/icons-material/Download';
 
 const Hero = () => {
-
+  
   return (
-    <main className='w-10/12 group relative hover:scale-105 h-auto hover:ease-in-out mt-32 mb-28  duration-200 transition-all transform  mx-auto  my-auto justify-center items-center rounded-3xl m-2 text-sm  bg-[#1a1a1a] p-2 shadow-2xl shadow-blur-12  shadow-red-500 border-8 border-red-500 md:space-y-6 md:items-center  md:px-16  z-40  flex flex-col overflow-x-hidden ' >
-      
+    <div className="flex h-screen group z-30 w-screen py-20 mb-64 relative hover:scale-105   hover:ease-in-out duration-200 transition-all transform ">
+          <div className='z-40   flex justify-center px-1 py-3 space-x-2 items-center mx-auto bg-[#1a1a1a] h-4 top-28 left-48 w-3/12 rounded-full absolute'>
+            
+            <span className="rounded-full bg-slate-600 w-12 h-2"></span>
+            
+            <span className="rounded-full bg-slate-600 w-2 h-2"></span>
+          </div>
+    <main className='w-10/12   relative hover:scale-105 h-screen  hover:ease-in-out sm:mt:0  mb-28    mx-auto  my-auto justify-center items-center rounded-3xl m-8 text-sm  bg-[#1a1a1a] p-1 shadow-2xl shadow-blur-12  shadow-red-500 border-4  border-red-500 md:space-y-6 md:items-center  md:px-16  z-30  flex flex-col  ' >
         {/* Hero text */}
-        <div className='h-full bottom-0 items-end justify-end rounded-3xl border-4 border-[#1a1a1a]  bg-[#f1f2f2]  w-full   pt-8 sm:py-24 text-slate-700  overflow-y-hidden   overflow-x-hidden p-4 flex flex-col'>
+        <div className='h-full bottom-0 z-40 relative items-end justify-center rounded-3xl border-4 border-[#1a1a1a]  bg-[#f1f2f2]  w-full    text-slate-700  overflow-y-hidden   overflow-x-hidden p-4 flex flex-col'>
           <h1 className='z-40 font-semibold   px-4 ml-4 md:w-full text-2xl sm:text-3xl   '>
               Hola, Bienvenidos!
           </h1>
@@ -37,10 +43,12 @@ const Hero = () => {
               </div>
             </div>
           </div>
-            <div className='w-6/12 pt-2 bottom-6 absolute h-2 mx-auto bg-gray-300 rounded-full'/>
+            <div className='w-6/12 pt-2 bottom-6 absolute z-40 h-2 mx-auto bg-gray-300 rounded-full'/>
       {/* </div> */}
       {/* <div className='h-2 w-8/12 flex flex-col absolute  bg-gray-400'/> */}
     </main>
+      <div className='h-full w-10/12 p-16 mx-2 mb-6 mt-2 top-17 shadow-4  shadow-xl  group-hover:scale-105  blur-sm border-8 bg-red-500 border-red-500 absolute -z-20  border-l-none border-b-none rounded-3xl   ml-10 lg:ml-28 lg:mb-62 lg:pl-4'/>
+      </div>
   );
 }
 
