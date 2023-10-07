e5b590c5d9b47d73d9bb4f55aaaeb0c9dcf3b328
ccastri <camilo.castrillon@uao.edu.co>
2023-10-07 17:00:00 -0500

    Now looks fine

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -8,23 +8,23 @@ const page = () => {
       <h2 className='my-8 text-25xl'>Conoce las diferentes opciones desde las cuales potencializar tu proyecto de salud con tecnología</h2>
 <div className='flex flex-col w-full space-y-6  '>
 
-      <div className='md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-400 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer'>
+      <div className='bg-[#fafafa] hover:text-[#fafafa] md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-600 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer'>
             <h2 className="text-md text-center font-bold tracking-wide">Desarrollo web (Sector salud y empresarial)</h2>
         <p className="text-xs text-center mx-auto w-full">La web es la manera más rapida de llegar a tus clientes. Construyamos una arquitectura que soporte y funcione 24/7 con un flujo abundante</p>
         </div>
-        <div className='md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-400 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer px-2'>
+        <div className='bg-[#fafafa] hover:text-[#fafafa] md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-600 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer px-2'>
             <h2 className="text-md text-center font-bold tracking-wide ">Consultoria IT en salud</h2>
         <p className="text-xs text-center mx-auto w-10/12">La web es la manera más rapida de llegar a tus clientes. Construyamos una arquitectura que soporte y funcione 24/7 con un flujo abundante</p>
         </div>
-        <div className='md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-400 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer px-2'>
+        <div className='bg-[#fafafa] hover:text-[#fafafa] md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-600 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer px-2'>
             <h2 className="text-md text-center font-bold tracking-wide ">Implementacion de servicios Smart FHIR en tu proyecto de salud</h2>
         <p className="text-xs text-center mx-auto w-10/12">La web es la manera más rapida de llegar a tus clientes. Construyamos una arquitectura que soporte y funcione 24/7 con un flujo abundante</p>
         </div>
-        <div className='md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-400 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer px-2'>
+        <div className='bg-[#fafafa] hover:text-[#fafafa] md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-600 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer px-2'>
             <h2 className="text-md text-center font-bold tracking-wide ">Integracion con servicios de historia clinica electrónica</h2>
         <p className="text-xs text-center mx-auto w-10/12">La web es la manera más rapida de llegar a tus clientes. Construyamos una arquitectura que soporte y funcione 24/7 con un flujo abundante</p>
         </div>
-        <div className='md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-400 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer px-2'>
+        <div className='bg-[#fafafa] hover:text-[#fafafa] md:w-6/12 py-6 space-y-4 flex flex-col mx-auto rounded-xl sm:w-8/12 w-auto hover:bg-green-600 transition-all hover:ease-in-out duration-200 transform hover:scale-105 cursor-pointer px-2'>
             <h2 className="text-md text-center font-bold tracking-wide ">Diseño 3D: modelado y planeación quirúrgica</h2>
         <p className="text-xs text-center mx-auto w-10/12">La web es la manera más rapida de llegar a tus clientes. Construyamos una arquitectura que soporte y funcione 24/7 con un flujo abundante</p>
         </div>
