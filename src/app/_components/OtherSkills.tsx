f35b65677929dee6614fad81bd1b9dd3270d6ab0
ccastri <camilo.castrillon@uao.edu.co>
2023-10-06 19:00:23 -0500

    Index done

--- src/app/_components/OtherSkills.tsx
+++ src/app/_components/OtherSkills.tsx
@@ -3,7 +3,7 @@ import CircularProgressBar from './progressBar';
 
 const OtherSkills = () => {
   return (
-    <div className=' h-full space-y-16 p-8 py-20 flex flex-col  bg-slate-300 w-screen items-center justify-center'>
+    <div className=' h-full space-y-16 p-8 py-20 flex flex-col  bg-[#F5F5F5] w-screen items-center justify-center'>
       <h1 className="text-2xl">Un poco de mi experiencia y habilidades...</h1>
       <div className="flex sm:flex-row space-y-16 flex-col">
 
@@ -28,7 +28,7 @@ const OtherSkills = () => {
         data={{
           labels: ["Ventas", "Digitalizacion", "Cronogramas de Mantenimiento"],
           values: [5, 15, 25],
-          bgColors: ["#dddddd", "#eeeeee", "#ffffff"],
+          bgColors: [" #8BC34A", "#4CAF50", "#388E3C"],
         }}
       />
       </div>
