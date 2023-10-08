9e3bccf977bdf3f88435e9e342f6829e73b20c46
ccastri <camilo.castrillon@uao.edu.co>
2023-10-08 00:32:17 -0500

    Products and previous github project fetched

--- src/app/_components/Repo.tsx
+++ src/app/_components/Repo.tsx
@@ -1,7 +1,10 @@
 'use client'
 import React, { FC, useEffect, useState } from 'react';
 import axios from 'axios';
-
+import StarOutlinedIcon from '@mui/icons-material/StarBorder';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import Image from 'next/image';
+// Lo voy a necesitar mas tarde
 // Define the Project interface here or import it from the appropriate location
 interface Project {
   id: number;
@@ -35,18 +38,23 @@ const Repo: FC = () => {
   }, []);
 
   return (
-    <div className="flex flex-col pt-20 bg-slate-800 py-16 text-[#f1f1f1] items-center space-y-2 h-screen ">
-      <h2 className="text-3xl font-bold">GitHub, mis repos preferidos y contribuciones OpenSource</h2>
+    <div className="flex flex-col pt-20 bg-slate-800 py-4 text-[#f1f1f1] items-center space-y-2 overflow-x-hidden w-screen h-full ">
+      <h2 className="mx-8 text-3xl font-bold relative z-30" >GitHub: repos preferidos aportes Open Source, etc...<span className="h-12 mr-4 mt-4 w-3 absolute z-40 bg-green-600"/></h2>
       <div 
-      className=' h-full w-screen grid grid-cols-2 gap-4   overflow-y-scroll  sm:grid-cols-3 lg:grid-cols-4 items-center justify-center'
+      className=' h-screen   sm:h-full w-screen grid grid-cols-2  sm:gap-4    overflow-y-scroll  sm:grid-cols-3 lg:grid-cols-4 lg:grid-rows-3 items-center justify-center'
       >
         {projects.map((project) => (
           <div 
-          className=" mx-auto border-4 h-full transition-all transform blur-sm hover:blur-0 shadow-4 shadow-slate-200 hover:shadow-xl hover:scale-105 hover:ease-in-out  duration-200 flex px-24 py-32 rounded-md w-full"
+          className=" m-4 border-4 h-full transition-all transform hover:blur-sm blur-0 shadow-4 shadow-slate-200 hover:shadow-xl hover:scale-105 hover:ease-in-out  duration-200 flex flex-col  rounded-md "
           key={project.id}>
-            <h2>
+            <h2 className='w-full top-0'>
               {project.name}
               </h2>
+              <div className="flex items-end justify-end space-x-4 bottom-2 h-full w-full right-2 overflow-x-hidden" >
+            <p><StarOutlinedIcon/> {project.stargazers_count}</p>
+            <div className="flex items-end justify-end"><Image src={"https://www.svgrepo.com/show/327279/git-branch.svg"} alt="" width={25} height={25}/> <>{project.forks_count}</></div>
+            <p><RemoveRedEyeIcon/>: {project.watchers_count}</p>
+              </div>
               </div>
         ))}
       </div>
