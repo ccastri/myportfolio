e4e992fcf1cb11fb757661c8af0d8bca2176d931
ccastri <camilo.castrillon@uao.edu.co>
2023-10-09 11:13:32 -0500

    Three js fully integrated import bug fixed _blank

--- src/app/products/(products)/ongoing-project/page.tsx
+++ src/app/products/(products)/ongoing-project/page.tsx
@@ -1,11 +1,10 @@
-'use client'
 import React from 'react'
 import StlViewerFiber from '@/app/_components/StlViewerFiber'
 const page = () => {
   return (
     <div className="mt-20 h-[calc(100vh-40px)]">
     <h1>Devs working here...
-Please, if you want to check the working component look at <span onClick ={()=>{window.open('http://3d-printer-k4h32pmvi.vercel.app')}}>HERE</span>
+Please, if you want to check the working component look at <span onClick ={()=>{window.open('http://3d-printer-k4h32pmvi.vercel.app', '_blank')}}>HERE</span>
 
     </h1>
       <StlViewerFiber/>
