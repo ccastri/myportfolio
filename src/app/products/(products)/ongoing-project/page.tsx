0f8008ab547434abd0a9f3cca340e019929acae0
ccastri <camilo.castrillon@uao.edu.co>
2023-10-09 11:22:10 -0500

    Three js window is not defined

--- src/app/products/(products)/ongoing-project/page.tsx
+++ src/app/products/(products)/ongoing-project/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import StlViewer from '@/app/_components/StlViewerFiber'
 const page = () => {
   return (
-    <div className="mt-20 h-[calc(100vh-40px)]">
+    <div className="">
     {/* <h1>Devs working here...
 Please, if you want to check the working component look at <span onClick ={()=>{window.open('http://3d-printer-k4h32pmvi.vercel.app', '_blank')}}>HERE</span>
 
